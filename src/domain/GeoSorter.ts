97f4da45bec56dfa25aedc74a49b54b32b8c066a
Rodrigo Cinto <rodcinto@gmail.com>
2024-02-09 11:44:51 +0100

    Integrate Salesman with Harvesine into GeoSorter.

--- src/domain/GeoSorter.ts
+++ src/domain/GeoSorter.ts
@@ -4,11 +4,19 @@ import { Haversine } from './Distance/strategies/Haversine.js';
 import { LocationType } from './Location.type.js';
 import { SortContext } from './Sorts/SortContext.js';
 import { BruteSort } from './Sorts/Strategies/BruteSort.js';
+import { Salesman } from './Sorts/Strategies/Salesman.js';
 
 export class GeoSorter {
   bruteSortLocationsWithHarvesine(unsortedList: List<LocationType>): List<LocationType> {
     const distanceContext = new DistanceContext(new Haversine());
     const sortContext = new SortContext(new BruteSort(distanceContext, unsortedList));
+
+    return sortContext.sortLocations();
+  }
+
+  sortWithSalesmanAndHarvesine(unsortedList: List<LocationType>): List<LocationType> {
+    const distanceContext = new DistanceContext(new Haversine());
+    const sortContext = new SortContext(new Salesman(distanceContext, unsortedList));
 
     return sortContext.sortLocations();
   }
